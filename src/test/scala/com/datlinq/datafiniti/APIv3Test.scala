7f61ed9982236586e9b53c5d0261be3ec54c7749
Tom Lous <tomlous@gmail.com>
2017-08-30 14:42:25 +0200

    token

--- src/test/scala/com/datlinq/datafiniti/APIv3Test.scala
+++ src/test/scala/com/datlinq/datafiniti/APIv3Test.scala
@@ -22,15 +22,16 @@ class APIv3Test extends fixture.FunSuite with PrivateMethodTester {
 
   test("private buildUrl") { apiv3 => {
     val buildUrl = PrivateMethod[String]('buildUrl)
+    val token = "-token-"
 
     def invoke(apiType: APIType, queryParts: Map[String, Any]): String = {
-      apiv3.invokePrivate(buildUrl(apiType, queryParts)).replace(apiv3.apiToken, "-token-")
+      apiv3.invokePrivate(buildUrl(apiType, queryParts)).replace(apiv3.apiToken, token)
     }
 
-    assert(invoke(Businesses, Map.empty[String, Any]) === "https://-token-:@api.datafiniti.co/v3/data/businesses")
-    assert(invoke(Products, List("a" -> 1).toMap) === "https://-token-:@api.datafiniti.co/v3/data/products?a=1")
-    assert(invoke(Products, List("a" -> 1, "b" -> None, "c" -> Some(true)).toMap) === "https://-token-:@api.datafiniti.co/v3/data/products?a=1&c=true")
-    assert(invoke(Businesses, List("view" -> "businesses_all", "format" -> "JSON", "q" -> Some("categories:hotels"), "records" -> 1, "download" -> false).toMap) === "https://-token-:@api.datafiniti.co/v3/data/businesses?format=JSON&q=categories:hotels&records=1&download=false&view=businesses_all")
+    assert(invoke(Businesses, Map.empty[String, Any]) === s"https://$token:@api.datafiniti.co/v3/data/businesses")
+    assert(invoke(Products, List("a" -> 1).toMap) === s"https://$token:@api.datafiniti.co/v3/data/products?a=1")
+    assert(invoke(Products, List("a" -> 1, "b" -> None, "c" -> Some(true)).toMap) === s"https://$token:@api.datafiniti.co/v3/data/products?a=1&c=true")
+    assert(invoke(Businesses, List("view" -> "businesses_all", "format" -> "JSON", "q" -> Some("categories:hotels"), "records" -> 1, "download" -> false).toMap) === s"https://$token:@api.datafiniti.co/v3/data/businesses?format=JSON&q=categories:hotels&records=1&download=false&view=businesses_all")
   }
   }
 
